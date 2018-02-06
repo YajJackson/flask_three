b03ceb252a9199e7feb8c29c4b4ae095f2eef3cc
Jay Jackson <jaynickjackson@gmail.com>
2018-02-06 16:41:39 -0500

    refactor usermodel implementation, refactor user resource

--- code/resources/user.py
+++ code/resources/user.py
@@ -13,13 +13,6 @@ class UserRegiser(Resource):
     if UserModel.find_by_username(data['username']):
       return {'message': 'A user with that username already exists'}, 400
       
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = "INSERT INTO users VALUES (NULL, ?, ?)"
-    cursor.execute(query, (data['username'], data['password']))
-
-    connection.commit()
-    connection.close()
+    UserModel(**data).save_to_db()
 
     return {'message': 'User created successfully.'}, 201
